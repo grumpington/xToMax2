1005322dce4382c455b5e559132d7085186ea2dd
grumpington <gaminggrump@gmail.com>
2019-08-10 00:14:26 +0200

    Add files via upload

---
+++ xToMax2.lua
@@ -0,0 +1,91 @@
+--Config Area
+
+local xToMax2Height = 28
+local xToMax2Width = 500
+local xToMax2Anchor = {"CENTER", UIPARENT, "CENTER", 0, -275}
+local xToMax2Point = {"CENTER", "xToMax2BarFrame","CENTER", 0, 0}
+local xToMax2Texture = "Interface\\TargetingFrame\\UI-StatusBar"
+local xToMax2Font = [[Fonts\FRIZQT__.TTF]]
+local xToMax2FontSize = 15
+local xToMax2FontFlags = "NONE"
+local xToMax2Color = { r = 0, g = 1, b = 0 }
+
+function comma_value(n)
+  return tostring(math.floor(n)):reverse():gsub("(%d%d%d)","%1,"):gsub(",(%-?)$","%1"):reverse()
+end
+
+--Creating the Frame
+local xToMax2BarFrame = CreateFrame("Frame", "xToMax2BarFrame", UIParent)
+xToMax2BarFrame:SetFrameStrata("HIGH")
+xToMax2BarFrame:SetHeight(xToMax2Height)
+xToMax2BarFrame:SetWidth(xToMax2Width)
+xToMax2BarFrame:SetPoint(unpack(xToMax2Anchor))
+xToMax2BarFrame:EnableMouse(true)
+xToMax2BarFrame:SetMovable(true)
+xToMax2BarFrame:SetClampedToScreen(true)
+
+--Creating background and border
+local backdrop = xToMax2BarFrame:CreateTexture(nil, "BACKGROUND")
+backdrop:SetHeight(xToMax2Height)
+backdrop:SetWidth(xToMax2Width)
+backdrop:SetPoint(unpack(xToMax2Point))
+backdrop:SetTexture(xToMax2Texture)
+backdrop:SetVertexColor(0.1, 0.1, 0.1)
+xToMax2BarFrame.backdrop = backdrop
+
+--Creating the XP Bar
+local xToMax2Bar = CreateFrame("StatusBar", "xToMax2Bar", xToMax2BarFrame)
+xToMax2Bar:SetWidth(xToMax2Width)
+xToMax2Bar:SetHeight(xToMax2Height)
+xToMax2Bar:SetPoint(unpack(xToMax2Point))
+xToMax2Bar:SetStatusBarTexture(xToMax2Texture)
+xToMax2Bar:GetStatusBarTexture():SetHorizTile(false)
+xToMax2Bar:SetStatusBarColor(xToMax2Color.r, xToMax2Color.g, xToMax2Color.b, 1)
+xToMax2BarFrame.XtoMax2Bar = xToMax2Bar
+
+--Creating the text on the bar
+local Text = xToMax2Bar:CreateFontString("xToMax2BarText", "OVERLAY")
+Text:SetFont(xToMax2Font, xToMax2FontSize, xToMax2FontFlags)
+Text:SetPoint("CENTER", xToMax2Bar, "CENTER",0,1)
+Text:SetAlpha(1)
+
+--Making the bar movable
+xToMax2BarFrame:SetScript("OnMouseDown", function(self, button)
+  if button == "LeftButton" and (IsShiftKeyDown()) and not self.isMoving then
+   self:StartMoving();
+   self.isMoving = true;
+   --print("moving!");
+  end
+end)
+
+xToMax2BarFrame:SetScript("OnMouseUp", function(self, button)
+  if button == "LeftButton" and (IsShiftKeyDown()) and self.isMoving then
+   self:StopMovingOrSizing();
+   self.isMoving = false;
+   --print("Stopped moving");
+  end
+end)
+
+--Making the magic Happen
+local function UpdateStatus()
+	local xToMax2XPLevel = {0, 400, 1300, 2700, 4800, 7600, 11200, 15700, 21100, 27600, 35200, 44000, 54100, 65500, 78400, 92800, 108800, 126500, 145900, 167200, 190400, 215600, 242900, 272300, 304000, 338000, 374400, 413300, 454700, 499000, 546400, 597200, 651700, 710300, 773100, 840200, 911800, 987900, 1068700, 1154400, 1245100, 1340900, 1441900, 1548200, 1660000, 1777500, 1900700, 2029800, 2164900, 2306100, 2453600, 2607500, 2767900, 2935000, 3108900, 3289700, 3477600, 3672600, 3874900};
+	local xToMax2CurrentXP = UnitXP("player") + xToMax2XPLevel[UnitLevel("player")]
+	local xToMax2MaxXP = 4084700
+	local xToMax2PercXP = floor(xToMax2CurrentXP/xToMax2MaxXP*100)
+	
+	if UnitLevel("player") == 60 then
+		backdrop:Hide()
+		xToMax2Bar:Hide()
+		xToMax2BarFrame:Hide()
+	else
+		xToMax2Bar:SetMinMaxValues(min(0, xToMax2CurrentXP), xToMax2MaxXP)
+		xToMax2Bar:SetValue(xToMax2CurrentXP)
+		Text:SetText(format("%s/%s (%s%%|cffb3e1ff|r)", comma_value(xToMax2CurrentXP), comma_value(xToMax2MaxXP), xToMax2PercXP))
+	end
+end
+
+--Register Events
+xToMax2BarFrame:RegisterEvent("PLAYER_LEVEL_UP")
+xToMax2BarFrame:RegisterEvent("PLAYER_XP_UPDATE")
+xToMax2BarFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+xToMax2BarFrame:SetScript("OnEvent", UpdateStatus)
