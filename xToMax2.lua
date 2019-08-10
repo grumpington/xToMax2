a2596adf7ff629e08dc2c450f58e86011c7c5616
grumpington <gaminggrump@gmail.com>
2019-08-10 22:30:36 +0200

    cleaned up the code a bit

--- xToMax2.lua
+++ xToMax2.lua
@@ -9,44 +9,10 @@ local xToMax2Font = [[Fonts\FRIZQT__.TTF]]
 local xToMax2FontSize = 12
 local xToMax2FontFlags = "NONE"
 local xToMax2Color = { r = 0, g = 1, b = 0 }
-local xToMax2SliderHeightName = "Height"
-local xToMax2SliderHeightTemplate = "OptionsSliderTemplate"
 
 function comma_value(n)
   return tostring(math.floor(n)):reverse():gsub("(%d%d%d)","%1,"):gsub(",(%-?)$","%1"):reverse()
 end
---[[
---here be the interface options panel
-xToMax2Panel = CreateFrame("Frame", "xToMax2Panel", UIParent);
-xToMax2Panel.name = "xToMax2";
-
---Making the sliders
-xToMax2HeightSlider = CreateFrame("Slider", xToMax2HeightSlider, xToMax2Panel, xToMax2SliderHeightTemplate)
-xToMax2HeightSlider:SetPoint("CENTER",0,0)
-xToMax2HeightSlider.textLow = _G[xToMax2SliderHeightName.."Low"]
-xToMax2HeightSlider.textHigh = _G[xToMax2SliderHeightName.."High"]
-xToMax2HeightSlider.text = _G[xToMax2SliderHeightName.."Text"]
-xToMax2HeightSlider:SetMinMaxValues(1,100)
-xToMax2HeightSlider.minValue, xToMax2HeightSlider.maxValue = xToMax2HeightSlider:GetMinMaxValues()
---xToMax2HeightSlider.textLow:SetText(xToMaxHeightSlider.minValue)
---xToMax2HeightSlider.textHigh:SetText(100)
---xToMax2HeightSlider.text:SetText(xToMax2SliderHeightName)
-xToMax2HeightSlider:SetValue(50)
-xToMax2HeightSlider:SetValueStep(1)
-xToMax2HeightSlider:SetScript("OnValueChanged", function(self,event,arg1) xToMax2ResizeHeight(event) end)
-
-InterfaceOptions_AddCategory(xToMax2Panel);
-
--- Functions to resize the bar
-function xToMax2ResizeHeight(value)
-	xToMax2Height = value
-end
-
-function xToMax2ResizeWidth(value)
-	xToMax2Width = value
-end
---]]
-
 
 --Creating the Frame
 local xToMax2BarFrame = CreateFrame("Frame", "xToMax2BarFrame", UIParent)
@@ -91,19 +57,7 @@ xToMax2BarFrame:SetScript("OnMouseDown", function(self, button)
 	if button == "LeftButton" then
 		self:StartMoving()
 		self.isMoving = true
-	--elseif button == "RightButton" then
-		--self:StartSizing("BOTTOMRIGHT")
-		--self.isSizing = true
 	end
-	
---[[
-  if button == "LeftButton" and (IsShiftKeyDown()) and not self.isMoving then
-   self:StartMoving();
-   self.isMoving = true;
-   --print("moving!");
-  end
-  
- --]]
 end)
 
 xToMax2BarFrame:SetScript("OnMouseUp", function(self, button)
